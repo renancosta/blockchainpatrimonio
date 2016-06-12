d9c4138693fe28af73bb91a94a6436e8cb1de453
Renan Costa <renan2727@hotmail.com>
2016-06-12 17:14:02 -0300

    Adequação de arquivo para o sistema de patrimônio e adicionando função para registro de atividades proposto por @alanMazuco

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,56 +1,59 @@
 var accounts;
 var account;
 var balance;
+var recordId;
+var recordDescription;
 
 function setStatus(message) {
-  var status = document.getElementById("status");
-  status.innerHTML = message;
+    var status = document.getElementById("status");
+    status.innerHTML = message;
 };
 
+//Atualização da página
 function refreshBalance() {
-  var meta = MetaCoin.deployed();
-
-  meta.getBalance.call(account, {from: account}).then(function(value) {
-    var balance_element = document.getElementById("balance");
-    balance_element.innerHTML = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error getting balance; see log.");
-  });
+    recordId = document.getElementById("id_patrimonio").value;
+    recordDescription = document.getElementById("description").value;
+    setStatus("Registro da Transação<br /><br />ID Patrimônio: " + recordId+"<br /> Descrição do repasse: "+ recordDescription);
+    writeLog();
 };
 
+//Capturando informações e submetendo a blockchain
 function sendCoin() {
-  var meta = MetaCoin.deployed();
 
-  var amount = parseInt(document.getElementById("amount").value);
-  var receiver = document.getElementById("receiver").value;
+    var meta = MetaCoin.deployed();
+    var amount = parseInt(document.getElementById("amount").value);
+    var receiver = document.getElementById("receiver").value;
 
-  setStatus("Initiating transaction... (please wait)");
+    setStatus("Inicializando a transacao... (espere)");
 
-  meta.sendCoin(receiver, amount, {from: account}).then(function() {
-    setStatus("Transaction complete!");
-    refreshBalance();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
-  });
-};
+    meta.sendCoin(receiver, amount, {from: account}).then(function() {
+        setStatus("Transacao completada!");
+        refreshBalance();
+        }).catch(function(e) {
+            console.log(e);
+	    setStatus("Erro ao realizar transação... (Verifique o log)");    
+        });
+     };
 
-window.onload = function() {
-  web3.eth.getAccounts(function(err, accs) {
-    if (err != null) {
-      alert("There was an error fetching your accounts.");
-      return;
-    }
+    window.onload = function() {
+        web3.eth.getAccounts(function(err, accs) {
+            if (err != null) {
+                alert("Houve um erro ao buscar suas contas.");
+                return;
+            }
 
-    if (accs.length == 0) {
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-      return;
-    }
+        if (accs.length == 0) {
+             alert("Não foi possível obter quaisquer contas! Verifique se o seu cliente Ethereum está configurado corretamente.");
+            return;
+        }
 
-    accounts = accs;
-    account = accounts[0];
+        accounts = accs;
+        account = accounts[0];
+        refreshBalance();
+    });
+}
 
-    refreshBalance();
-  });
+//Registrando log independente
+function writeLog(){
+    console.log(recordId+recordDescription);
 }
